dd4937a6ccfc0def387f6202f61473cbe95ab255
Mavlin Dm <mavlind@list.ru>
2023-05-07 17:36:32 +0700

    add jexcel

--- src/pages/auto/Input-Data.vue
+++ src/pages/auto/Input-Data.vue
@@ -2,7 +2,8 @@
 	<layout>
 		<q-page class="q-px-lg">
 			<h2 class="q-ml-xl text-primary">Ввод данных</h2>
-<!--			<article-list :list="store.state?.articles?.items" />-->
+			<!--			<article-list :list="store.state?.articles?.items" />-->
+			<div id="spreadsheet"></div>
 		</q-page>
 	</layout>
 </template>
@@ -10,12 +11,28 @@
 <script setup>
 
 	import Layout from '@/layouts/default.vue'
-	// import ArticleList from '@/components/ArticlesList.vue'
 	import {useStore} from 'vuex'
 	import {setTitle} from '@/middleware'
+	import {onMounted} from 'vue'
+	import jspreadsheet from 'jspreadsheet-ce'
 
 	const store = useStore()
-
+	const options = {
+		data: [[]],
+		minDimensions: [10, 10],
+	}
+	const el = 'spreadsheet'
 	setTitle(`Ввод данных`)
 
+	onMounted(async () => {
+
+		let spreadsheet = jspreadsheet(document.getElementById(el), options)
+
+	})
 </script>
+<style lang="scss">
+
+	@import '@/css/jexcel.css';
+	//@import 'src/css/jsuites.css';
+
+</style>
